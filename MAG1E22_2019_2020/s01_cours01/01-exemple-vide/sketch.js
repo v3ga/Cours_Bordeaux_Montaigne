c13d317d4a057a2cbd4e2edc95f194c3d174067f
v3ga <julien@v3ga.net>
2019-09-25 21:18:11 +0200

    MAG1E22

---
+++ MAG1E22_2019_2020/s01_cours01/01-exemple-vide/sketch.js
@@ -0,0 +1,12 @@
+// Démarrage du sketch
+function setup() 
+{
+	// Création de l'élément canvas, taille 500 x 500 pixels
+	createCanvas(500,500);
+}
+
+// Dessin sur l'élément <canvas>
+function draw() 
+{
+	ellipse(250,250,100,100);
+}
